baca80820d6ccf7fbda8b9de705b69b348ff99e9
Rewalon <rewalon@hotmail.com>
2019-09-16 20:34:41 +0500

    First commin the project. Find files in table. Size and data create.

---
+++ edf/FindForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace create_update
+{
+    partial class FindForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.FileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tbFolderPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnStartFind = new System.Windows.Forms.Button();
+            this.lblCountFindedFiles = new System.Windows.Forms.Label();
+            this.chkSelectAllFiles = new System.Windows.Forms.CheckBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnSelectDirectory = new System.Windows.Forms.Button();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView.CheckBoxes = true;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FileName,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.Location = new System.Drawing.Point(3, 28);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(1047, 518);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // FileName
+            // 
+            this.FileName.Text = "Name of Files";
+            this.FileName.Width = 335;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 144;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Width = 152;
+            // 
+            // tbFolderPath
+            // 
+            this.tbFolderPath.Location = new System.Drawing.Point(12, 12);
+            this.tbFolderPath.Name = "tbFolderPath";
+            this.tbFolderPath.Size = new System.Drawing.Size(390, 20);
+            this.tbFolderPath.TabIndex = 1;
+            this.tbFolderPath.Text = "c:\\iac";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Укажите папку с файлами обновления";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // btnStartFind
+            // 
+            this.btnStartFind.Location = new System.Drawing.Point(408, 39);
+            this.btnStartFind.Name = "btnStartFind";
+            this.btnStartFind.Size = new System.Drawing.Size(75, 23);
+            this.btnStartFind.TabIndex = 3;
+            this.btnStartFind.Text = "&Find";
+            this.btnStartFind.UseVisualStyleBackColor = true;
+            this.btnStartFind.Click += new System.EventHandler(this.BtnStartFind_Click);
+            // 
+            // lblCountFindedFiles
+            // 
+            this.lblCountFindedFiles.AutoSize = true;
+            this.lblCountFindedFiles.Location = new System.Drawing.Point(12, 44);
+            this.lblCountFindedFiles.Name = "lblCountFindedFiles";
+            this.lblCountFindedFiles.Size = new System.Drawing.Size(30, 13);
+            this.lblCountFindedFiles.TabIndex = 4;
+            this.lblCountFindedFiles.Text = "Find ";
+            // 
+            // chkSelectAllFiles
+            // 
+            this.chkSelectAllFiles.AutoSize = true;
+            this.chkSelectAllFiles.Location = new System.Drawing.Point(2, 8);
+            this.chkSelectAllFiles.Margin = new System.Windows.Forms.Padding(2, 8, 2, 0);
+            this.chkSelectAllFiles.Name = "chkSelectAllFiles";
+            this.chkSelectAllFiles.Size = new System.Drawing.Size(91, 17);
+            this.chkSelectAllFiles.TabIndex = 5;
+            this.chkSelectAllFiles.Text = "Выбрать все";
+            this.chkSelectAllFiles.UseVisualStyleBackColor = true;
+            this.chkSelectAllFiles.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "*.lst";
+            this.saveFileDialog1.RestoreDirectory = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.chkSelectAllFiles, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.listView, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 83);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 238F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1053, 549);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // btnSelectDirectory
+            // 
+            this.btnSelectDirectory.Image = global::create_update.Properties.Resources.icon_folder_open;
+            this.btnSelectDirectory.Location = new System.Drawing.Point(408, 10);
+            this.btnSelectDirectory.Name = "btnSelectDirectory";
+            this.btnSelectDirectory.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectDirectory.TabIndex = 2;
+            this.btnSelectDirectory.Text = "&Directory";
+            this.btnSelectDirectory.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSelectDirectory.UseCompatibleTextRendering = true;
+            this.btnSelectDirectory.UseVisualStyleBackColor = true;
+            this.btnSelectDirectory.Click += new System.EventHandler(this.BtnSelectDirectory_Click);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Width = 156;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Width = 171;
+            // 
+            // FindForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 632);
+            this.Controls.Add(this.lblCountFindedFiles);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnStartFind);
+            this.Controls.Add(this.btnSelectDirectory);
+            this.Controls.Add(this.tbFolderPath);
+            this.MinimumSize = new System.Drawing.Size(696, 388);
+            this.Name = "FindForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "win.edf";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader FileName;
+        private System.Windows.Forms.TextBox tbFolderPath;
+        private System.Windows.Forms.Button btnSelectDirectory;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnStartFind;
+        private System.Windows.Forms.Label lblCountFindedFiles;
+        private System.Windows.Forms.CheckBox chkSelectAllFiles;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ edf/FindForm.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+
+namespace create_update
+{
+    public partial class FindForm : Form
+    {
+        public FindForm()
+        {
+            InitializeComponent();
+        }
+
+     
+        /// <summary>
+        /// Показывает или скрывает панель ожидания.
+        /// </summary>
+        /// <param name="b"><see langword="true"/> Показать панель.; <see langword="false"/> Скрыть панель.</param>
+        private void BlockHeadControls(Boolean b)
+        {
+            // Обрабатываем кнопки.
+            btnStartFind.Enabled = !b;
+            chkSelectAllFiles.Enabled = !b;
+           
+            // Устанавливаем курсор.
+            if (b)
+            {
+                Cursor = Cursors.WaitCursor;
+            }
+            else
+            {
+                Cursor = Cursors.Default;
+            }
+        }
+
+        private void BtnSelectDirectory_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbFolderPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void BtnStartFind_Click(object sender, EventArgs e)
+        {            
+            BlockHeadControls(true);
+
+            try
+            {
+                string path = tbFolderPath.Text;
+
+                if (!Directory.Exists(path))
+                {
+                    MessageBox.Show(
+                        "Указанный путь не существует. Поиск невозможен.",
+                        "Ошибка имени пути", 
+                        MessageBoxButtons.OK);
+
+                    return;
+                }
+
+                //todo: Insert check filename 
+
+                // Получаем все файлы.
+                string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+                FillListFiles(files);
+            }
+            finally
+            {
+                BlockHeadControls(false);
+
+                // Выводим информацию о найденых файлах.
+                lblCountFindedFiles.Text = string.Format("Find {0} file(s)", listView.Items.Count);
+                // Устанавливаем параметры общего выделения.
+                CheckAllFiles(false);
+            }
+        }
+
+        private void FillListFiles(string[] files)
+        {
+            // Отключаем обновление списка.
+            listView.BeginUpdate();
+            // Очищаем список.
+            listView.Items.Clear();
+
+            // Перебор полученных файлов.
+            foreach (string file in files)
+            {
+                FileInfo fileInf = new FileInfo(file);
+                if (fileInf.Exists)
+                {
+                   
+                }
+
+                ListViewItem lvi = new ListViewItem
+                {
+                    // установка названия файла.
+                    Text = file,
+                    //   lvi.SubItems.Add(file.Length.ToString());
+                    // Установка картинки для файла.
+                    ImageIndex = 0                
+                };
+                lvi.SubItems.Add(fileInf.Length.ToString());
+                lvi.SubItems.Add(fileInf.CreationTime.ToString());
+                lvi.SubItems.Add(fileInf.LastWriteTime.ToString());
+                // Добавляем элемент в ListView.
+                listView.Items.Add(lvi);
+            }
+
+            // Включаем обновление списка.
+            listView.EndUpdate();
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            BlockHeadControls(true);
+
+            CheckAllFiles(chkSelectAllFiles.Checked);
+
+            BlockHeadControls(false);
+        }
+
+        /// <summary>
+        /// Установка или снятие флага отметки файлов в списке.
+        /// </summary>
+        /// <param name="checkAll"><see langword="true"/>Отметить все.<see langword="false"/>Снять отметку у всех.</param>
+        private void CheckAllFiles(bool checkAll)
+        {
+            // Меняем статус checkBox в зависимости от задачи.
+            chkSelectAllFiles.Text = (checkAll) ? "Снять чек со всех" : "Выбрать все";
+            chkSelectAllFiles.CheckState = (checkAll) ? CheckState.Checked: CheckState.Unchecked;
+
+            listView.BeginUpdate();
+            
+            // Обходим список и устанавливаем/снимаем флаг.
+            foreach (ListViewItem l in listView.Items)
+            {
+                l.Checked = checkAll;
+            }              
+            
+            listView.EndUpdate();
+        }
+
+        /// <summary>
+        /// Получение количества отмеченных файлов.
+        /// </summary>
+        /// <param name="list">ListView</param>
+        /// <returns></returns>
+        private int CountCheckedFiles(ListView list)
+        {
+            int count = 0;
+            foreach (ListViewItem l in list.Items)
+            {
+                if (l.Checked)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Получаем список отмеченных файлов.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        private string[] GetCheckFiles(ListView list)
+        {
+            var fileLists = new string[CountCheckedFiles(listView)];
+            int count = 0;
+            foreach (ListViewItem l in listView.Items)
+            {
+                if (l.Checked)
+                {
+                    fileLists[count] = l.Text;
+                    count++;
+                }
+            }
+            return fileLists;
+
+        }
+
+        
+
+    }
+}
